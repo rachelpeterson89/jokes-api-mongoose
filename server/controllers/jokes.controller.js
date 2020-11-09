b27578124dd1016620e9bdd897d8cc6d27db0d5d
Rachel Peterson <rachelp5446@gmail.com>
2020-11-09 15:26:55 -0600

    first commit

---
+++ server/controllers/jokes.controller.js
@@ -0,0 +1,37 @@
+const Joke = require("../models/jokes.model");
+
+module.exports.findAllJokes = (req, res) => {
+  Joke.find()
+    .then(allMyJokes => res.json({ jokes: allMyJokes }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.findOneSingleJoke = (req, res) => {
+	Joke.findOne({ _id: req.params.id })
+		.then(oneSingleJoke => res.json({ joke: oneSingleJoke }))
+		.catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.randomJoke = (req, res) => {
+  Joke.findOneRandom({ $sample: req.{ size: 1 } })
+  .then(oneRandomJoke => res.json({ joke: oneRandomJoke }) )
+  .catch(err => res.json({ message: "Something went wrong", error: err }));
+}
+
+module.exports.createNewJoke = (req, res) => {
+  Joke.create(req.body)
+    .then(newlyCreatedJoke => res.json({ joke: newlyCreatedJoke }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.updateExistingJoke = (req, res) => {
+  Joke.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(updatedJoke => res.json({ joke: updatedJoke }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.deleteExistingJoke = (req, res) => {
+  Joke.deleteOne({ _id: req.params.id })
+    .then(result => res.json({ result: result }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
---
+++ server/routes/jokes.routes.js
@@ -0,0 +1,10 @@
+const JokeController = require("../controllers/jokes.controller");
+
+module.exports = app => {
+  app.get("/api/jokes/", JokeController.findAllJokes);
+  app.get("/api/jokes/:id", JokeController.findOneSingleJoke);
+  app.get("/api/jokes/random", JokeController.randomJoke);
+  app.post("/api/jokes/new", JokeController.createNewJoke);
+  app.put("/api/jokes/update/:id", JokeController.updateExistingJoke);
+  app.delete("/api/jokes/delete/:id", JokeController.deleteExistingJoke);
+};
